7a76180cb92d0eed3f5ea2a83b7b119088e79905
cw353 <77987696+cw353@users.noreply.github.com>
2024-06-12 19:25:55 -0400

    Add files for building pdf (pdf-specific toc and build script)

---
+++ makepdf.sh
@@ -0,0 +1,39 @@
+#!/bin/sh -x
+
+# define variables
+srcdir="src"
+latexdir="src/_build/latex"
+parentdir="../../.."
+
+# back up original _toc.yml
+mv "$srcdir"/_toc.yml "$srcdir"/_toc-original.yml
+
+# set PDF-specific _toc.yml
+cp "$srcdir"/_toc-pdf.yml "$srcdir"/_toc.yml
+
+# compile LaTeX (and do so twice to fix cross-references etc.)
+for i in {1..2}
+do
+  jb build "$srcdir" --builder latex
+done
+
+cd "$latexdir"
+
+# do initial build of pdf
+xelatex -interaction=nonstopmode JNBbook
+
+# make index (manually replacing \spxentry with \indexentry)
+makeindex JNBbook
+sed -i '' 's/spxentry/sphinxstyleindexentry/g' JNBbook.ind
+sed -i '' 's/spxextra/sphinxstyleindexextra/g' JNBbook.ind
+
+# rebuild pdf (and do so twice to fix index etc.)
+for i in {1..2}
+do
+  xelatex -interaction=nonstopmode JNBbook
+done
+
+cd "$parentdir"
+
+# restore original _toc.yml
+mv "$srcdir"/_toc-original.yml "$srcdir"/_toc.yml
